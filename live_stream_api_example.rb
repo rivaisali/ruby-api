941ada82cbf4d89503316b8215f0ef1df6289d74
Alex Titze <mail@alextitze.com>
2016-03-03 11:58:11 -0800

    updated Wowza class comment

--- live_stream_api_example.rb
+++ live_stream_api_example.rb
@@ -1,5 +1,5 @@
 /**
- * <<<live_stream_example.rb>>> and all components (c) Copyright 2006 - 2016, Wowza Media Systems, LLC.  All rights reserved.  This <<module>> is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
+ * live_stream_example.rb and all components (c) Copyright 2006 - 2016, Wowza Media Systems, LLC.  All rights reserved.  This class is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
  */
 
 #!/usr/bin/env ruby
