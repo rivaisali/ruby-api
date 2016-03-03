48dd8a25b68065295278d5ebc401f2287d9ba1e4
Alex Titze <mail@alextitze.com>
2016-03-03 11:57:13 -0800

    added Wowza class comment

--- live_stream_api_example.rb
+++ live_stream_api_example.rb
@@ -1,3 +1,7 @@
+/**
+ * <<<live_stream_example.rb>>> and all components (c) Copyright 2006 - 2016, Wowza Media Systems, LLC.  All rights reserved.  This <<module>> is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
+ */
+
 #!/usr/bin/env ruby
 
 require 'yaml'
